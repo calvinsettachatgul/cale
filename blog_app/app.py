1fe34150efe9e403df394d38e494d6ca404e36d3
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-12-06 04:04:07 +0000

    add users route

--- blog_app/app.py
+++ blog_app/app.py
@@ -32,7 +32,11 @@ def all_articles():
 def get_article(id):
     article = articles[id]
     return render_template('articles.html',article=article)
-
+    
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = ["user1", "user2", "user3"]
+    return render_template('users.html', users=users)
     
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080, debug=True)
